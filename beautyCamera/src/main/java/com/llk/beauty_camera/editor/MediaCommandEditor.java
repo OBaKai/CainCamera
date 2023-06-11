f99a8a554c084035dfb3ed5fd968f1dfa47ab7b3
liangliangkai <liangkaileong@foxmail.com>
2023-06-11 23:22:05 +0800

    android api合并逻辑（视频慢帧bug）

--- beautyCamera/src/main/java/com/llk/beauty_camera/editor/MediaCommandEditor.java
+++ beautyCamera/src/main/java/com/llk/beauty_camera/editor/MediaCommandEditor.java
@@ -1,8 +1,11 @@
 package com.llk.beauty_camera.editor;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.media.MediaCodec;
 import android.media.MediaExtractor;
 import android.media.MediaFormat;
+import android.media.MediaMuxer;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.HandlerThread;
@@ -10,9 +13,13 @@ import android.text.TextUtils;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.llk.beauty_camera.BuildConfig;
 
 import java.io.File;
 import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -23,6 +30,7 @@ import java.util.Locale;
 public class MediaCommandEditor {
 
     private static final String TAG = MediaCommandEditor.class.getSimpleName();
+    private static final boolean isDebug = BuildConfig.DEBUG;
 
     private Handler mHandler;
 
@@ -47,6 +55,132 @@ public class MediaCommandEditor {
         }
         mHandler.getLooper().quitSafely();
         mHandler = null;
+    }
+
+    @SuppressLint("WrongConstant")
+    public void execMuxer(@NonNull String outputPath,
+                          @NonNull String videoPath,
+                          @NonNull String audioPath,
+                          @Nullable CommandProcessCallback callback){
+        mHandler.post(() -> { //丢到子线程处理
+            MediaMuxer muxer = null;
+            MediaExtractor audioExtractor = null;
+            MediaExtractor videoExtractor = null;
+            long startTime = 0;
+            try{
+                if (isDebug) startTime = System.currentTimeMillis();
+
+                muxer = new MediaMuxer(outputPath, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+
+                int audioTrackIndex = -1;
+                int audioMuxerTrackIndex = -1;
+                int audioMaxInputSize = 0;
+                audioExtractor = new MediaExtractor();
+                audioExtractor.setDataSource(audioPath);
+                int audioTrackCount = audioExtractor.getTrackCount();
+                for (int i=0; i<audioTrackCount; i++){
+                    MediaFormat format = audioExtractor.getTrackFormat(i);
+                    String mine = format.getString(MediaFormat.KEY_MIME);
+                    if (mine.startsWith("audio/")){
+                        audioTrackIndex = i;
+                        audioMuxerTrackIndex = muxer.addTrack(format); //将音轨添加到MediaMuxer，并返回新的轨道
+                        audioMaxInputSize = format.getInteger(MediaFormat.KEY_MAX_INPUT_SIZE);
+                        break;
+                    }
+                }
+                if (isDebug){
+                    Log.d(TAG, "find audioMuxerTrackIndex -> " + audioMuxerTrackIndex);
+                }
+
+                int videoTrackIndex = -1;
+                int videoMuxerTrackIndex = -1;
+                int videoMaxInputSize = 0;
+                int videoFrameRate = 0;
+                videoExtractor = new MediaExtractor();
+                videoExtractor.setDataSource(videoPath);
+                int videoTrackCount = videoExtractor.getTrackCount();
+                for (int i=0; i<videoTrackCount; i++){
+                    MediaFormat format = videoExtractor.getTrackFormat(i);
+                    String mine = format.getString(MediaFormat.KEY_MIME);
+                    if (mine.startsWith("video/")){
+                        videoTrackIndex = i;
+                        videoMuxerTrackIndex = muxer.addTrack(format); //将视频轨添加到MediaMuxer，并返回新的轨道
+                        videoMaxInputSize = format.getInteger(MediaFormat.KEY_MAX_INPUT_SIZE); //得到能获取的有关视频的最大值
+                        videoFrameRate = format.getInteger(MediaFormat.KEY_FRAME_RATE); //获取视频的帧率
+                        break;
+                    }
+                }
+                if (isDebug){
+                    Log.d(TAG, "find videoMuxerTrackIndex -> " + videoMuxerTrackIndex);
+                }
+
+                muxer.start(); //开始合成
+
+                audioExtractor.selectTrack(audioTrackIndex);
+                MediaCodec.BufferInfo audioMediaInfo = new MediaCodec.BufferInfo();
+                ByteBuffer audioBuffer = ByteBuffer.allocate(audioMaxInputSize);
+                while (true) {
+                    int sampleSize = audioExtractor.readSampleData(audioBuffer, 0); //检索当前编码的样本并将其存储在字节缓冲区中
+                    if (sampleSize <= 0) {
+                        audioExtractor.unselectTrack(audioTrackIndex);
+                        break;
+                    }
+
+                    //设置样本编码信息
+                    audioMediaInfo.offset = 0;
+                    audioMediaInfo.size = sampleSize;
+                    audioMediaInfo.flags = audioExtractor.getSampleFlags();
+                    audioMediaInfo.presentationTimeUs = audioExtractor.getSampleTime();
+
+                    muxer.writeSampleData(audioMuxerTrackIndex, audioBuffer, audioMediaInfo);
+
+                    audioExtractor.advance();
+                }
+
+                videoExtractor.selectTrack(videoTrackIndex); //将提供视频图像的视频选择到视频轨上
+                MediaCodec.BufferInfo videoMediaInfo = new MediaCodec.BufferInfo();
+                ByteBuffer videoBuffer = ByteBuffer.allocate(videoMaxInputSize);
+                while (true) {
+                    int sampleSize = videoExtractor.readSampleData(videoBuffer, 0); //检索当前编码的样本并将其存储在字节缓冲区中
+                    if (sampleSize <= 0) {
+                        videoExtractor.unselectTrack(videoTrackIndex);
+                        break;
+                    }
+
+                    //设置样本编码信息
+                    videoMediaInfo.offset = 0;
+                    videoMediaInfo.size = sampleSize;
+                    videoMediaInfo.flags = videoExtractor.getSampleFlags();
+                    videoMediaInfo.presentationTimeUs += 1000 * 1000 / videoFrameRate;
+
+                    muxer.writeSampleData(videoMuxerTrackIndex, videoBuffer, videoMediaInfo);
+
+                    videoExtractor.advance();
+                }
+                if (callback != null){
+                    callback.onProcessResult(0);
+                }
+            }catch (Exception e){
+                Log.e(TAG, "execMuxer fail, err=" + e.getLocalizedMessage());
+                e.printStackTrace();
+                if (callback != null){
+                    callback.onProcessResult(-1);
+                }
+            }finally {
+                if (muxer != null){
+                    muxer.stop();
+                    muxer.release();
+                }
+
+                if (audioExtractor != null) audioExtractor.release();
+                if (videoExtractor != null) videoExtractor.release();
+
+                if (isDebug){
+                    long duration = System.currentTimeMillis() - startTime;
+                    Log.e(TAG, "execMuxer finish, duration=" + duration);
+                }
+            }
+        });
     }
 
     /**
